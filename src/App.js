79bc969c9f4e0a42e04e02ed354da258eaad4c00
Soryu-u <collages447@gmail.com>
2024-10-14 13:52:20 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import styles from'./App.module.css';
+import Registration from './Pages/Registration/Pegistration';
+
+function App() {
+  return (
+    <div className={styles.main}>
+      <Registration/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Registration/Pegistration.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Registration.module.css";
+
+export default function Registration() {
+    const page = "Зареєструватись";
+    function Submit() {
+        return console.log("submit")
+    };
+
+    return (
+        <div className={styles.main}>
+            <div className={styles.head}>
+                <p>Sloboda and Dragons</p>
+            </div>
+            <div className={styles.container}>
+                <p className={styles.header}>Реєстрація</p>
+                <div>
+                    <div className={styles.data_inputs}>
+                        <div>
+                            <p>Ім'я користувача</p>
+                            <input className={styles.input}/>
+                        </div>
+                        <div>
+                            <p>Пароль</p>
+                            <input className={styles.input}/>
+                        </div>
+                    </div>
+                    <button className={styles.register_btn} type="submit" title={page} onClick={Submit}>
+                        {page}
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
