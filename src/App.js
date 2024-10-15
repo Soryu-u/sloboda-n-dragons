e10c73597baabfeddd313fe1957987c6bd674c45
Soryu-u <collages447@gmail.com>
2024-10-15 11:15:52 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Outlet, Route, Router, Routes } from 'react-router-dom';
 import styles from'./App.module.css';
+import Main from './Pages/Main/Main';
 import Registration from './Pages/Registration/Pegistration';
 
 function App() {
-  return (
-    <div className={styles.main}>
-      <Registration/>
-    </div>
-  );
+    return (
+		<div className={styles.main}>
+			<Routes>
+				<Route path="/" element={<Main/>}/>
+				<Route path="registration" element={<Registration/>}/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
